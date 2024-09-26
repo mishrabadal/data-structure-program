05b3dc8931e95c4de164540bea88e48b0f5ade6b
badal <mishrabadal988@gmail.com>
2024-09-26 19:55:58 +0530

    recursion program added

---
+++ Recursion/recursion_basic.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+using namespace std;
+
+void happy_birthday(int n)
+{
+    if (n == 0)
+    {
+        cout << n << " happy birthday !";
+        return;
+    }
+    cout << n << " day left in birthday" << endl;
+    happy_birthday(n - 1);
+}
+
+void natural_number(int n)
+{
+    if (n == 0)
+    {
+
+        return;
+    }
+    cout << n << " ";
+    natural_number(n - 1);
+}
+
+// int factorial(int n)
+// {
+//     if (n > 0)
+//     {
+
+//         return n * factorial(n - 1);
+//     }
+//     else
+//     {
+//         return 1;
+//     }
+// }
+
+int factorial(int n)
+{
+    if (n == 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int sum_of_digit(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    return n % 10 + sum_of_digit(n / 10);
+}
+
+void print_1_to_n(int num)
+{
+    if (num == 1)
+    {
+        cout << num << " ";
+        return;
+    }
+    print_1_to_n(num - 1);
+    cout << num << " ";
+}
+
+void even_print(int n)
+{
+    if (n == 0)
+    {
+        cout << 0 << " ";
+        return;
+    }
+    even_print(n - 2);
+    cout << n << " ";
+}
+
+int sum_1_to_n(int n)
+{
+    if (n == 1)
+        return 1;
+
+    return n + sum_1_to_n(n - 1);
+}
+
+int square_sum(int n)
+{
+    if (n == 1)
+        return 1;
+
+    return n * n + square_sum(n - 1);
+}
+
+int power_of_2(int n)
+{
+    if (n == 1)
+        return 2;
+
+    return 2 * power_of_2(n - 1);
+}
+
+int nth_fibonacci(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+
+    return nth_fibonacci(n - 1) + nth_fibonacci(n - 2);
+}
+
+int main()
+{
+    cout << nth_fibonacci(10);
+    return 0;
+}
