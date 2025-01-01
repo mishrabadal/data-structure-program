70140a0321d6c701b46cab14d18f658a6bd6f218
badal <mishrabadal988@gmail.com>
2025-01-01 12:19:27 +0530

    Maximum of minimum for every window size

---
+++ STACK/stack_6.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stack>
+using namespace std;
+
+//Maximum of minimum for every window size
+void maxOfMin()
+{
+    // Your code here
+    vector<int> arr = {10, 20, 30, 50, 10, 70, 30};
+    int n = arr.size();
+    vector<int> ans(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            int num = INT_MAX;
+            for (int k = j; k < i + 1 + j; k++)
+            {
+                num = min(num, arr[k]);
+            }
+            ans[i] = max(ans[i], num);
+        }
+    }
+    for (auto i : ans)
+        cout << i << " ";
+}
+
+int main()
+{
+    
+    maxOfMin();
+    return 0;
+}
