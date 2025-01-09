b95236018333dc2cfadff3bb4d956e22f1a25924
badal <mishrabadal988@gmail.com>
2025-01-09 12:31:47 +0530

    count leaf node

--- TREE/tempCodeRunnerFile.cpp
+++ TREE/tempCodeRunnerFile.cpp
@@ -1,6 +1,6 @@
-int total_sum(node *root)
-// {
-//     if (root == NULL)
-//         return 0;
-//     return (root->data + total_sum(root->left) + total_sum(root->right));
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
