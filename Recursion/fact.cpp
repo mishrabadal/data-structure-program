07c5cb1048d706bf9ebff49d611d78c6d71ef3a3
badal <mishrabadal988@gmail.com>
2025-01-17 23:42:19 +0530

    left view of binary tree

--- Recursion/fact.cpp
+++ Recursion/fact.cpp
@@ -1,23 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-//factorial
-void fact(int n){
-    int res=1;
-    for (int i = n; i >=1; i--)
+#include <math.h>
+#include <vector>
+#include <algorithm>
+// factorial
+void fact(int n)
+{
+    int res = 1;
+    for (int i = n; i >= 1; i--)
     {
-        res =res*i;
+        res = res * i;
     }
-    cout<<res;
-
+    cout << res;
 }
 
-int factorial(int n){
-    if(n==1)
-    return 1;
-    return n*factorial(n-1);
+int factorial(int n)
+{
+    if (n == 1)
+        return 1;
+    return n * factorial(n - 1);
 }
 
-int main(){
-   cout<<factorial(4);
+void nextGreatest(vector<int> arr)
+{
+    vector<int> ans;
+    int n = arr.size();
+    int mx = -1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int temp = mx;
+        if (arr[i] > mx)
+        {
+            temp = arr[i];
+        }
+        arr[i] = mx;
+        mx = temp;
+    }
+    for(auto i :arr)
+    cout<<i<<" ";
+}
+
+int main()
+{
+    vector<int> arr = {16, 17, 4, 3, 5, 2};
+   nextGreatest( arr);
     return 0;
 }
