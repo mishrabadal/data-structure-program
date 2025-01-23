80009d807e82d8af14303920899f0ad7a264bd07
badal <mishrabadal988@gmail.com>
2025-01-24 00:49:26 +0530

    make number from array

--- Logical_question/logical_question.cpp
+++ Logical_question/logical_question.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+#include <math.h>
+
 bool prime_number()
 {
     int n = 9;
@@ -52,62 +54,75 @@ void fibonacci()
         y = z;
         z = x + y;
     }
-
 }
 
-void leap_year_checker(){
-int year = 2020;
-if(year%400 ==0)
-cout<<"leap year";
+void leap_year_checker()
+{
+    int year = 2020;
+    if (year % 400 == 0)
+        cout << "leap year";
 
-if(year%100 ==0)
-cout<<"not leap year";
+    if (year % 100 == 0)
+        cout << "not leap year";
 
-if(year%4==0)
-cout<<"yes leap year";
+    if (year % 4 == 0)
+        cout << "yes leap year";
 
-else 
-cout<<"not leap year";
-
+    else
+        cout << "not leap year";
 }
 
+// C++ program to print all prime factors
+// A function to print all prime
+// factors of a given number n
+void primeFactors(int n)
+{
+    // Print the number of 2s that divide n
+    while (n % 2 == 0)
+    {
+        cout << 2 << " ";
+        n = n / 2;
+    }
 
+    // n must be odd at this point. So we can skip
+    // one element (Note i = i +2)
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        // While i divides n, print i and divide n
+        while (n % i == 0)
+        {
+            cout << i << " ";
+            n = n / i;
+        }
+    }
 
-// C++ program to print all prime factors 
-// A function to print all prime 
-// factors of a given number n 
-void primeFactors(int n) 
-{ 
-	// Print the number of 2s that divide n 
-	while (n % 2 == 0) 
-	{ 
-		cout << 2 << " "; 
-		n = n/2; 
-	} 
-
-	// n must be odd at this point. So we can skip 
-	// one element (Note i = i +2) 
-	for (int i = 3; i <= sqrt(n); i = i + 2) 
-	{ 
-		// While i divides n, print i and divide n 
-		while (n % i == 0) 
-		{ 
-			cout << i << " "; 
-			n = n/i; 
-		} 
-	} 
-
-	// This condition is to handle the case when n 
-	// is a prime number greater than 2 
-	if (n > 2) 
-		cout << n << " "; 
-} 
+    // This condition is to handle the case when n
+    // is a prime number greater than 2
+    if (n > 2)
+        cout << n << " ";
+}
 
 /* Driver code */
-int main() 
-{ 
-	int n = 315; 
-	primeFactors(n); 
-	return 0; 
-} 
 
+// make number from array
+void make_num()
+{
+    int num[] = {1, 9, 3, 4};
+    int ans = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        ans = ans * 10 + num[i];
+    }
+    cout << ans;
+}
+int main()
+{
+    int a = 2, b = 5, c = 8, d = 3;
+    int ans;
+    ans = a * 10 + b;
+    ans = ans * 10 + c;
+    ans = ans * 10 + d;
+   make_num();
+
+    return 0;
+}
