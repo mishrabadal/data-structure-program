886767ac0ad8bf15c20048883988e4481f74ff8f
badal <mishrabadal988@gmail.com>
2025-03-10 11:39:55 +0530

    BST INSERTION

--- BST/demo.cpp
+++ BST/demo.cpp
@@ -1,12 +1,60 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int fact(int n){
-    if(n==1)
-    return 1;
-return n*fact(n-1);
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node(int value)
+    {
+        data = value;
+        left = right = NULL;
+    }
+};
+Node *insert(Node *root, int target)
+{
+    if (!root)
+    {
+        Node *temp = new Node(target);
+        return temp;
+    }
+    // left side
+    if (target < root->data)
+    {
+        root->left = insert(root->left, target);
+    }
+    // right side
+    else
+    {
+        root->right = insert(root->right, target);
+    }
+    return root;
 }
-int main(){
-    cout<<fact(5);
-    cout<<"hello world";
+void in_order(Node *root)
+{
+
+    if (root == NULL)
+        return;
+    in_order(root->left);
+    cout << root->data << " ";
+    in_order(root->right);
+}
+
+//default bst create
+//T(n)=O(h)insert single element in worst case;
+void bst()
+{
+    int arr[] = {3, 7, 4, 1, 6, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Node *root = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        root = insert(root, arr[i]);
+    }
+    in_order(root);
+}
+int main()
+{
+bst();
     return 0;
 }
